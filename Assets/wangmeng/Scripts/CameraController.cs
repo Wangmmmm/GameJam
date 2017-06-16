deca025057ff231dbbe579df8fa18c63d2d6924c
wmmmm <1229819460@qq.com>
2017-06-16 21:08:37 +0800

    dotween 插件和手势管理插件添加

---
+++ Assets/wangmeng/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform PlayerPos;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
